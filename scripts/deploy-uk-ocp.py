3afd2352fc99bf540806d1194645056028ae26e4
alfonsomthd <alfonsomthd@protonmail.com>
2024-05-23 16:00:02 +0200

    Separate config from code.

--- scripts/deploy-uk-ocp.py
+++ scripts/deploy-uk-ocp.py
@@ -2,26 +2,39 @@ import schedule
 import datetime
 import time
 import os
+import yaml
+
+with open('./config/env.yaml', 'r') as env_file:
+    environment = yaml.safe_load(env_file)
+
 def get_suffix():
     nowtime = datetime.datetime.now()
     month = nowtime.strftime("%b").lower()
     day = str(nowtime.day)
     return month + '-' + day
 
-def job(t):
+def job():
     try:
         print("executing")
         suffix = get_suffix()
-        os.system(f"deploy-ocp  --email-ids almartin@redhat.com,ialmeida@redhat.com,badhikar@redhat.com,nthomas@redhat.com,skatiyar@redhat.com --ocp4mcoci-conf samples/deploy_ocp_cluster/override_config.yaml --cluster-name odfcluster-uk-{suffix} --cluster-path /tmp/odfcluster-uk-{suffix}")
+        deploy_cmd = (f"deploy-ocp --ocp4mcoci-conf {environment['ocp_config']}"
+            f" --cluster-name odfcluster-uk-{suffix} --cluster-path /tmp/odfcluster-uk-{suffix}"
+        )
+        if 'webhook_url' in environment:
+            deploy_cmd += f" --webhook-url '{environment['webhook_url']}'"
+        if 'email_ids' in environment:
+            deploy_cmd += f" --email-ids {environment['email_ids']}"
+
+        os.system(deploy_cmd)
     except Exception:
         pass
 
-for i in ["11:30"]:
-    schedule.every().monday.at(i).do(job, i)
-    schedule.every().tuesday.at(i).do(job, i)
-    schedule.every().wednesday.at(i).do(job, i)
-    schedule.every().thursday.at(i).do(job, i)
-    schedule.every().friday.at(i).do(job, i)
+for i in [environment['schedule_time_deploy']]:
+    schedule.every().monday.at(i).do(job)
+    schedule.every().tuesday.at(i).do(job)
+    schedule.every().wednesday.at(i).do(job)
+    schedule.every().thursday.at(i).do(job)
+    schedule.every().friday.at(i).do(job)
 
 while True:
     schedule.run_pending()
